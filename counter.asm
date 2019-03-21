b85cf827e26fa015495a711e6c9452d5313023a7
Mustafa Asaad <mostafa0as3d@gmail.com>
2019-03-21 12:02:34 +0200

    Pushing final tweaks!

--- counter.asm
+++ counter.asm
@@ -1,54 +1,70 @@
 $include (c8051f020.inc)
 
+ORG 0
+
 TABLE EQU 100H
 
 
-PING      	 EQU   P1.0
-PINF		 		 EQU	 P1.1
-PINA			 	 EQU	 P1.2
-PINB			 	 EQU	 P1.3
-PINE		 		 EQU	 P1.4
-PIND		 		 EQU	 P1.5
-PINC			 	 EQU	 P1.6
-PINDOT			 EQU	 P1.7
-
-ORG 0
+; Enable the Port I/O Crossbar
+			MOV WDTCN, #0DEh
+      MOV WDTCN, #0ADh
+      MOV XBR2, #40H
+			ORL P1MDIN, #0FFH
+      ORL P1MDOUT,#0FFH
+			MOV A, #0FFH
+			MOV P2, A
+			
 
 MAIN: 
-; Enable the Port I/O Crossbar
-      MOV   XBR2, #40h
-			ORL   P1MDIN, #40h	 
-      ORL   P1MDOUT,#40h 
-			
-			
 			MOV A,#00H
+			MOV P2,A
 			MOV DPTR,#TABLE
 			MOV R0,A
 
-
-
-		
-
-LOOP: MOVC A,@A+DPTR
+LOOP: 
+			MOVC A,@A+DPTR
 			MOV P1,A
 			INC R0
 			MOV A,R0
 			CALL DELAY
-			CJNE R0,#0AH,LOOP
-
-
 			
-	JMP MAIN
 
-DELAY:
-				MOV R1,#00H
-				MOV R2,#00H
-				MOV R3,#05H
-				LOOP1:
-								DJNZ R1,LOOP1
-								DJNZ R2,LOOP1
+
+DELAY:	
+				MOV R1,#50
+				MOV R2,#30
+				MOV R5,#10
+				MOV R4,#255
+				MOV R3,#255
+				JB P2.0,LOOP1
+				JB P2.1,LOOP2
+				JB P2.2,LOOP3
+				LOOP1:	
+								DJNZ R3,LOOP1
+								DJNZ R4,LOOP1
+								DJNZ R5,LOOP1
+								CJNE R0,#0AH,LOOP
+      
+								JMP MAIN
 								
-				RET
+				LOOP2:
+								DJNZ R3,LOOP2
+								DJNZ R4,LOOP2
+								DJNZ R1,LOOP2
+								CJNE R0,#0AH,LOOP
+      
+								JMP MAIN
+				LOOP3:	
+								DJNZ R3,LOOP3
+								DJNZ R4,LOOP3
+								DJNZ R2,LOOP3
+								CJNE R0,#0AH,LOOP
+      
+								JMP MAIN
+								
+RET
+
+
 
 			ORG TABLE
 			DB 3FH,06H,5BH,4FH,66H,6DH,7DH,07H,7FH,6FH
